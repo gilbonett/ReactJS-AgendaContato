71c75a992fcc590c8f62a0c638e5beae1c434d73
Pedro Bonett <gilbonett@gmail.com>
2022-08-09 18:18:41 -0300

    Criando estrutura do  Projeto ContactDirectory

---
+++ src/components/FormContact/FormContact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+ const FormContact = () => {
+    return (
+        <div>
+            <h1>
+                Inicializando Projeto
+            </h1>
+        </div>
+      );
+ }
+  
+ export default FormContact ;
